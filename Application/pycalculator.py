aef79fcda18c16b152c3c353c1250769561c52cc
Harsh Lad <harshlad07@gmail.com>
2024-02-04 11:07:49 +0530

    0004 Running Calculator
    Signed-off-by: Harsh Lad <harshlad07@gmail.com>

--- Application/pycalculator.py
+++ Application/pycalculator.py
@@ -1,13 +1,44 @@
 """Simple calculator built with Python and PyQt"""
 import sys
+from functools import partial
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout,  QLineEdit, QPushButton, QVBoxLayout
 
 ERROR_MSG = "ERROR"
-WINDOW_SIZE = 435
+WINDOW_SIZE = 235
 DISPLAY_SIZE = 35
 BUTTON_SIZE = 40
 
+"""Calculator model"""
+class PyCalc:
+    """PyCalc's controller class."""
+
+    def __init__(self, model, view):
+        self._evaluate = model
+        self._view = view   
+        self._connectSignalsAndSlots()
+
+    def _calculateResult(self):
+        result = self._evaluate(expression=self._view.displayText())
+        self._view.setDisplayText(result)
+
+    def _buildExpression(self, subExpression):
+        if self._view.displayText() == ERROR_MSG:
+            self._view.clearDisplay()
+        expression = self._view.displayText() + subExpression
+        self._view.setDisplayText(expression)
+
+    def _connectSignalsAndSlots(self):
+        for keySymbol, button in self._view.buttonMap.items():
+            if keySymbol not in {"=", "C"}:
+                button.clicked.connect(
+                    partial(self._buildExpression, keySymbol)
+                )
+        self._view.buttonMap["="].clicked.connect(self._calculateResult)
+        self._view.display.returnPressed.connect(self._calculateResult)
+        self._view.buttonMap["C"].clicked.connect(self._view.clearDisplay)
+    
+"""UI functions"""
 class PyCalcWindow(QMainWindow):
     """Main window"""
     def __init__(self):
@@ -73,6 +104,7 @@ def main():
     pycalcApp = QApplication([])
     pycalcWindow = PyCalcWindow()
     pycalcWindow.show()
+    PyCalc(model=evaluateExpression, view=pycalcWindow)
     sys.exit(pycalcApp.exec())
 
 """To run the application, just run this file"""
